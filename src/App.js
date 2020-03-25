0125c1597df7e0dce5834985d83c635cada21b8b
Sambalicious <stemitope370@gmail.com>
2020-03-25 20:26:08 +0100

    added more components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
+import { Test } from './components/Test';
 
 
 
@@ -8,10 +10,16 @@ import { Home } from './components/Home';
 
 function App() {
   return(
+   
     <div className="container mx-auto ">
-          <Header />
-          <Home />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Header} />
+          <Route path="/test" exact component={Test}/>
+        </Switch>
+      </Router>
     </div>
+    
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return (
@@ -15,6 +16,36 @@ export const Header = () => {
           <p>it will only take a moment to answer some to our questions so as to provide you with best analysis provided by NDCDC guidelines</p>
         </div>
        
-      </div>
+     
+
+       <Fragment>
+       <div className="max-w-sm rounded overflow-hidden shadow-lg">
+     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+       <div className="mb-4">
+         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+           Username
+         </label>
+         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+       </div>
+       <div className="mb-6">
+         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+           Date of Birth
+         </label>
+         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="date" placeholder="Enter your date of birth"/>
+         
+       </div>
+       <div className="flex items-center justify-between">
+         <Link to="/test" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+           Proceed
+         </Link>
+         
+       </div>
+     </form>
+     <p className="text-center text-gray-500 text-xs">
+       &copy;2020 E-SAM. All rights reserved.
+     </p>
+   </div>       
+          </Fragment>
+    </div>
     )
 }
