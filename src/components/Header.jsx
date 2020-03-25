bf5a468db8a9551679fb48d5827fd2428f2f47c1
Sambalicious <stemitope370@gmail.com>
2020-03-25 19:52:40 +0100

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+       
+         <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-green-500 text-4xl mb-2">
+              COVID-19 Tracker
+          </div>
+          <p className="text-gray-700 text-base">
+           Do you feel different and you are curios to know your COVID-19 status? 
+          </p>
+          <h4 className="font-bold text-green-500 text-xl mb-2"> We gat you.</h4>
+          <p>it will only take a moment to answer some to our questions so as to provide you with best analysis provided by NDCDC guidelines</p>
+        </div>
+       
+      </div>
+    )
+}
