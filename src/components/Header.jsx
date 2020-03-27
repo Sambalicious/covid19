4c3b49d0f34b8724a65e6636e74d958a8da0a1cc
Sambalicious <stemitope370@gmail.com>
2020-03-27 23:58:11 +0100

    more commits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React, {Fragment, useState} from 'react';
 import { Link } from 'react-router-dom';
-
+import covid from '../utils/images/covid.jpeg';
 export const Header = () => {
 
-    const [name, setName] = useState('');
+   
+
+   
     return (
        
          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+           <img className="w-full" src={covid} alt="images" />
         <div className="px-6 py-4">
           <div className="font-bold text-green-500 text-4xl mb-2">
               COVID-19 Tracker
@@ -22,27 +25,14 @@ export const Header = () => {
 
        <Fragment>
        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-     <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-           Username
-         </label>
-         <input value={name} onChange={(e)=> setName(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
-       </div>
-       <div className="mb-6">
-         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-           Date of Birth
-         </label>
-         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="date" placeholder="Enter your date of birth"/>
-         
-       </div>
+     
        <div className="flex items-center justify-between">
          <Link to="/test" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
            Proceed
          </Link>
          
        </div>
-     </form>
+
      <p className="text-center text-gray-500 text-xs">
        &copy;2020 E-SAM. All rights reserved.
      </p>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -7,9 +7,8 @@ const Questions = ({title, pics, score }) => {
        
     return (
           <div className="max-w-sm rounded overflow-hidden shadow-lg">
-               <img className="w-full" src={pics} alt="images"/>
+               <img className="w-medium" src={pics} alt="images"/>
                  <div className="px-6 py-4">
-                     {score}
           <div  className="font-bold text-xl mb-2">{title}</div>
         </div>
         <div className="px-6 py-4">
